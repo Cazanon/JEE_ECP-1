3167ee6148a7f7a607fefcb321931f743543baf3
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-11 14:13:33 +0100

    Add clase DaoJpaFactory

---
+++ src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
@@ -0,0 +1,42 @@
+package persistence.models.daos.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+
+import persistence.models.daos.VotoDao;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.UserDao;
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "tictactoe";
+
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+    @Override
+    public UserDao getUserDao() {
+        return new UserDaoJpa();
+    }
+
+    @Override
+    public TemaDao getTemaDao() {
+        return new TemaDaoJpa();
+    }
+
+    @Override
+    public VotoDao getVotoDao() {
+        return new VotoDaoJpa();
+    }
+
+}
