7bea2eef7311214331bd2e72ba1f28c7eb0bb1c4
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-11 13:30:34 +0100

    Add clase Tema

---
+++ src/main/java/persistence/models/entities/Tema.java
@@ -0,0 +1,53 @@
+package persistence.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Tema {
+	
+	@Id
+    @GeneratedValue
+    private Integer id;
+	
+	private String name;
+	
+	public Tema(String name) {
+		this.name = name;
+	}
+	
+	public Tema() {
+    }
+	
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+    public boolean equals(Object obj) {
+        assert obj != null;
+        Tema other = (Tema) obj;
+        boolean result = id.equals(other.id) && name.equals(other.name);
+        
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + "]";
+    }
+
+}
