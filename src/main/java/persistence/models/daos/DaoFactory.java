152d4a65bdb27f1292c28fb2278dfadb28b3a974
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-11 14:05:28 +0100

    Add clase DaoFactory

---
+++ src/main/java/persistence/models/daos/DaoFactory.java
@@ -0,0 +1,21 @@
+package persistence.models.daos;
+
+public abstract class DaoFactory {
+    public static DaoFactory factory = null;
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract UserDao getUserDao();
+
+    public abstract TemaDao getTemaDao();
+
+    public abstract VotoDao getVotoDao();
+
+}
