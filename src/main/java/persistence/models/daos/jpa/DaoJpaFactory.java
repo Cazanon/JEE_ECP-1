9b1e1d6ae8e3b0747c8c4d79dd2bc926fb91e412
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-12 09:37:31 +0100

    Add clase UserDaoJpaTest. No se ha comprobado si funciona o no.

--- src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
@@ -1,9 +1,13 @@
 package persistence.models.daos.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.logging.log4j.LogManager;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 import persistence.models.daos.VotoDao;
 import persistence.models.daos.TemaDao;
@@ -11,32 +15,40 @@ import persistence.models.daos.DaoFactory;
 import persistence.models.daos.UserDao;
 
 public class DaoJpaFactory extends DaoFactory {
-    private static final String PERSISTENCE_UNIT = "tictactoe";
+	private static final String PERSISTENCE_UNIT = "tictactoe";
 
-    private static EntityManagerFactory entityManagerFactory = Persistence
-            .createEntityManagerFactory(PERSISTENCE_UNIT);
+	private static EntityManagerFactory entityManagerFactory = Persistence
+			.createEntityManagerFactory(PERSISTENCE_UNIT);
 
-    public DaoJpaFactory() {
-        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
-    }
+	public DaoJpaFactory() {
+		LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+	}
 
-    public static EntityManagerFactory getEntityManagerFactory() {
-        return entityManagerFactory;
-    }
+	public static void dropAndCreateTables() {
+		Map<String, String> properties = new HashMap<>();
+		properties.put(PersistenceUnitProperties.DDL_GENERATION,
+				PersistenceUnitProperties.DROP_AND_CREATE);
+		entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+		LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+	}
 
-    @Override
-    public UserDao getUserDao() {
-        return new UserDaoJpa();
-    }
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
 
-    @Override
-    public TemaDao getTemaDao() {
-        return new TemaDaoJpa();
-    }
+	@Override
+	public UserDao getUserDao() {
+		return new UserDaoJpa();
+	}
 
-    @Override
-    public VotoDao getVotoDao() {
-        return new VotoDaoJpa();
-    }
+	@Override
+	public TemaDao getTemaDao() {
+		return new TemaDaoJpa();
+	}
+
+	@Override
+	public VotoDao getVotoDao() {
+		return new VotoDaoJpa();
+	}
 
 }
