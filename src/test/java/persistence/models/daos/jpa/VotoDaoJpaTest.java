2ff7c5351383f4bb80ff549c157d6f491e31abb5
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-18 13:29:00 +0100

    Update VotoDaoJpaTest (reajuste del diseño)

--- src/test/java/persistence/models/daos/jpa/VotoDaoJpaTest.java
+++ src/test/java/persistence/models/daos/jpa/VotoDaoJpaTest.java
@@ -10,8 +10,6 @@ import org.junit.Test;
 
 import persistence.models.daos.DaoFactory;
 import persistence.models.daos.VotoDao;
-import persistence.models.entities.Tema;
-import persistence.models.entities.User;
 import persistence.models.entities.Voto;
 import persistence.models.utils.NivelEstudios;
 
@@ -29,8 +27,6 @@ public class VotoDaoJpaTest {
     @Before
     public void before() {
         this.voto = new Voto(5, "ipUsuario", NivelEstudios.PRIMARIOS);
-        this.voto.setUser(new User("user","pass"));
-        this.voto.setTema(new Tema("name"));
         dao = DaoFactory.getFactory().getVotoDao();
         dao.create(voto);
     }
@@ -42,12 +38,9 @@ public class VotoDaoJpaTest {
 
     @Test
     public void testUpdateCategory() {
-        voto.setEscala(6);
+        voto.setValoracion(6);
         voto.setIpUsuario("other");
         voto.setNivelEstudios(NivelEstudios.SECUNDARIOS);
-        voto.getUser().setName("other");
-        voto.getUser().setPassword("other");
-        voto.getTema().setName("other");
         dao.update(voto);
         assertEquals(voto, dao.read(voto.getId()));
     }
@@ -56,15 +49,11 @@ public class VotoDaoJpaTest {
     public void testDeleteByID() {
         dao.deleteById(voto.getId());
         assertNull(dao.read(voto.getId()));
-        assertNull(DaoFactory.getFactory().getUserDao().read(voto.getUser().getId()));
-        assertNull(DaoFactory.getFactory().getTemaDao().read(voto.getTema().getId()));
     }
 
     @Test
     public void testFindAll() {        
         this.voto = new Voto(8, "ipUsuario2", NivelEstudios.UNIVERSITARIOS);
-        this.voto.setUser(new User("user2","pass2"));
-        this.voto.setTema(new Tema("name2"));
         dao = DaoFactory.getFactory().getVotoDao();
         dao.create(voto);
         assertEquals(2, dao.findAll().size());
