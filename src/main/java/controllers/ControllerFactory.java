68131df491722d8c95db2d3f7cec95762ced6e5c
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-23 16:01:48 +0100

    Update ControllerFactory (se instancia por inyección, igual que DaoFactory)

--- src/main/java/controllers/ControllerFactory.java
+++ src/main/java/controllers/ControllerFactory.java
@@ -1,6 +1,16 @@
 package controllers;
 
 public abstract class ControllerFactory {
+	public static ControllerFactory factory = null;
+
+    public static void setFactory(ControllerFactory factory) {
+    	ControllerFactory.factory = factory;
+    }
+
+    public static ControllerFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
 	
 	public abstract AnyadirTemaController getAnyadirTemaController();
 
