09cb543efe82e455f7907fd30d68efe79c792d2d
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 12:15:11 +0100

    Add VerVotacionesBean (hereda de TemaBean)

---
+++ src/main/java/views/beans/VerVotacionesBean.java
@@ -0,0 +1,15 @@
+package views.beans;
+
+import java.io.Serializable;
+
+public class VerVotacionesBean extends TemaBean implements Serializable, GenericBean {
+	private static final long serialVersionUID = 1L;
+
+    public VerVotacionesBean() {
+    }
+
+	@Override
+	public void process() {
+	}
+	
+}
