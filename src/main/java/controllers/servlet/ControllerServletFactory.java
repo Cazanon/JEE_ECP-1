b71537160f2750d7ff97c32e1b976debe21dfc0d
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 10:21:26 +0100

    Update ControllerServletFactory (TemaController)

--- src/main/java/controllers/servlet/ControllerServletFactory.java
+++ src/main/java/controllers/servlet/ControllerServletFactory.java
@@ -3,26 +3,36 @@ package controllers.servlet;
 import controllers.AnyadirTemaController;
 import controllers.ControllerFactory;
 import controllers.EliminarTemaController;
+import controllers.TemaController;
 
 public class ControllerServletFactory extends ControllerFactory {
-	
+
 	private AnyadirTemaController anyadirTemaController;
 	private EliminarTemaController eliminarTemaController;
+	private TemaController temaController;
 
 	@Override
 	public AnyadirTemaController getAnyadirTemaController() {
 		if (this.anyadirTemaController == null) {
 			this.anyadirTemaController = new AnyadirTemaServletController();
-			}
-			return anyadirTemaController;
+		}
+		return anyadirTemaController;
 	}
 
 	@Override
 	public EliminarTemaController getEliminarTemaController() {
 		if (this.eliminarTemaController == null) {
 			this.eliminarTemaController = new EliminarTemaServletController();
-			}
-			return eliminarTemaController;
+		}
+		return eliminarTemaController;
+	}
+
+	@Override
+	public TemaController getTemaController() {
+		if (this.temaController == null) {
+			this.temaController = new TemaServletController();
+		}
+		return temaController;
 	}
 
 }
