d863128f88bc85e9dafa015bd87921c49d28ed99
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 09:36:47 +0100

    Update ControllerServletFactory (se añade el controlador de eliminar tema) 	Add EliminarTemaServletController

--- src/main/java/controllers/servlet/ControllerServletFactory.java
+++ src/main/java/controllers/servlet/ControllerServletFactory.java
@@ -2,10 +2,12 @@ package controllers.servlet;
 
 import controllers.AnyadirTemaController;
 import controllers.ControllerFactory;
+import controllers.EliminarTemaController;
 
 public class ControllerServletFactory extends ControllerFactory {
 	
 	private AnyadirTemaController anyadirTemaController;
+	private EliminarTemaController eliminarTemaController;
 
 	@Override
 	public AnyadirTemaController getAnyadirTemaController() {
@@ -13,6 +15,14 @@ public class ControllerServletFactory extends ControllerFactory {
 			this.anyadirTemaController = new AnyadirTemaServletController();
 			}
 			return anyadirTemaController;
+	}
+
+	@Override
+	public EliminarTemaController getEliminarTemaController() {
+		if (this.eliminarTemaController == null) {
+			this.eliminarTemaController = new EliminarTemaServletController();
+			}
+			return eliminarTemaController;
 	}
 
 }
---
+++ src/main/java/controllers/servlet/EliminarTemaServletController.java
@@ -0,0 +1,22 @@
+package controllers.servlet;
+
+import java.util.List;
+
+import persistence.models.entities.Tema;
+import controllers.EliminarTemaController;
+
+public class EliminarTemaServletController implements EliminarTemaController {
+
+	@Override
+	public void eliminarTema(Tema tema) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Tema> obtenerListaTemas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
