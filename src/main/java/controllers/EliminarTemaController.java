5409bb99d9bbe3459ea7957302ba474c4880229d
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 09:33:04 +0100

    Update EliminarTemaController (se añaden los métodos; estructura parecida al controlador de añadir, a lo mejor se puede hacer herencia)

--- src/main/java/controllers/EliminarTemaController.java
+++ src/main/java/controllers/EliminarTemaController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.util.List;
+
+import persistence.models.entities.Tema;
+
 public interface EliminarTemaController {
+
+	void eliminarTema(Tema tema);
+
+	List<Tema> obtenerListaTemas();
 
 }
