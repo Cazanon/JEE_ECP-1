6e043dbeb9bf68e63092aac6fd9ae07b9b1a01e9
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 09:47:02 +0100

    Update AnyadirTemaController (hereda de la interfaz TemaController)

--- src/main/java/controllers/AnyadirTemaController.java
+++ src/main/java/controllers/AnyadirTemaController.java
@@ -1,13 +1,9 @@
 package controllers;
 
-import java.util.List;
-
 import persistence.models.entities.Tema;
 
 public interface AnyadirTemaController {
 
 	void anyadirTema(Tema tema);
-	
-	List<Tema> obtenerListaTemas();
 	
 }
