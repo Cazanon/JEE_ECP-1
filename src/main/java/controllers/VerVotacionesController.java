0eed13ba787c6b05ea41070eb9a761f3d1ce5428
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 12:55:52 +0100

    Update interface VerVotacionesController (método obtenerListaVotosPorTema)

--- src/main/java/controllers/VerVotacionesController.java
+++ src/main/java/controllers/VerVotacionesController.java
@@ -1,5 +1,11 @@
 package controllers;
 
+import java.util.List;
+
+import persistence.models.entities.Voto;
+
 public interface VerVotacionesController {
+	
+	List<Voto> obtenerListaVotosPorTema();
 
 }
