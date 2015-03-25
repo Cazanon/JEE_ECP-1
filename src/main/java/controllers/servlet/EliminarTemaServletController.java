927bded4cfec64ccde4930356ae898a9576ecf43
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 09:53:44 +0100

    Update EliminarTemaServletController (hereda de TemaServletController)

--- src/main/java/controllers/servlet/EliminarTemaServletController.java
+++ src/main/java/controllers/servlet/EliminarTemaServletController.java
@@ -1,25 +1,16 @@
 package controllers.servlet;
 
-import java.util.List;
-
 import persistence.models.daos.DaoFactory;
 import persistence.models.daos.jpa.DaoJpaFactory;
 import persistence.models.entities.Tema;
 import controllers.EliminarTemaController;
 
-public class EliminarTemaServletController implements EliminarTemaController {
+public class EliminarTemaServletController extends TemaServletController implements EliminarTemaController {
 
 	@Override
 	public void eliminarTema(Tema tema) {
 		DaoFactory.setFactory(new DaoJpaFactory());
     	DaoFactory.getFactory().getTemaDao().deleteById(tema.getId());
-	}
-
-	@Override
-	public List<Tema> obtenerListaTemas() {
-		DaoFactory.setFactory(new DaoJpaFactory());
-    	List<Tema> listaTemas = DaoFactory.getFactory().getTemaDao().findAll();
-    	return listaTemas;
 	}
 
 }
