01d1b19a10e72a8f22137841055e821ad50051fe
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-25 09:46:33 +0100

    Update EliminarTemaController (hereda de la interfaz TemaController)

--- src/main/java/controllers/EliminarTemaController.java
+++ src/main/java/controllers/EliminarTemaController.java
@@ -1,13 +1,9 @@
 package controllers;
 
-import java.util.List;
-
 import persistence.models.entities.Tema;
 
-public interface EliminarTemaController {
+public interface EliminarTemaController extends TemaController {
 
 	void eliminarTema(Tema tema);
-
-	List<Tema> obtenerListaTemas();
 
 }
