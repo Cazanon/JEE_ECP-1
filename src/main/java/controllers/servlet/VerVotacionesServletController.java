cdae99a43123a543190f3a15aba4a4542678f367
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-26 13:35:33 +0100

    Update VerVotacionesServletController

--- src/main/java/controllers/servlet/VerVotacionesServletController.java
+++ src/main/java/controllers/servlet/VerVotacionesServletController.java
@@ -1,18 +1,23 @@
 package controllers.servlet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import persistence.models.daos.DaoFactory;
 import persistence.models.daos.jpa.DaoJpaFactory;
-import persistence.models.entities.Voto;
+import persistence.models.entities.Tema;
 import controllers.VerVotacionesController;
 
 public class VerVotacionesServletController implements VerVotacionesController {
 
 	@Override
-	public List<Voto> obtenerListaVotosPorTema(Integer id) {
+	public List<Integer> obtenerListaVotosPorTema(List<Tema> temas) {
+		List<Integer> numeroVotosPorTema = new ArrayList<Integer>();
 		DaoFactory.setFactory(new DaoJpaFactory());
-    	return DaoFactory.getFactory().getTemaDao().read(id).getVotos();
+		for(Tema tema : temas) {
+			numeroVotosPorTema.add(tema.getVotos().size());
+		}
+    	return numeroVotosPorTema;
 	}
 
 }
