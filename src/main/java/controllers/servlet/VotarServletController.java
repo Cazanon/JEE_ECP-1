b420f346cee7bb29369f782305d8c4177609291c
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-26 12:43:38 +0100

    Update VotarServletController (método votar)

--- src/main/java/controllers/servlet/VotarServletController.java
+++ src/main/java/controllers/servlet/VotarServletController.java
@@ -1,5 +1,7 @@
 package controllers.servlet;
 
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.jpa.DaoJpaFactory;
 import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
 import controllers.VotarController;
@@ -8,8 +10,9 @@ public class VotarServletController implements VotarController {
 
 	@Override
 	public void votar(Tema tema, Voto voto) {
-		// TODO Auto-generated method stub
-
+		tema.setVoto(voto);
+		DaoFactory.setFactory(new DaoJpaFactory());
+    	DaoFactory.getFactory().getTemaDao().update(tema);
 	}
 
 }
