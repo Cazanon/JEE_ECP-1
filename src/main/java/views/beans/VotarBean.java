851d00a54fae2fa39ce185a1f295ea4f829998cc
xtsopr26 <xtsopr26@OEPMPC3143.oepm.local>
2015-03-26 13:14:56 +0100

    Update VotarBean (desarrollo completa; pendiente Dispatcher y probar)

--- src/main/java/views/beans/VotarBean.java
+++ src/main/java/views/beans/VotarBean.java
@@ -2,11 +2,18 @@ package views.beans;
 
 import java.io.Serializable;
 
+import persistence.models.entities.Voto;
+import persistence.models.utils.NivelEstudios;
+import controllers.ControllerFactory;
+import controllers.servlet.ControllerServletFactory;
+
 public class VotarBean extends TemaBean implements Serializable, GenericBean {
 
 	private static final long serialVersionUID = 1L;
 	
 	private int valoracion;
+	
+	private Integer id;
 
 	public int getValoracion() {
 		return valoracion;
@@ -15,9 +22,20 @@ public class VotarBean extends TemaBean implements Serializable, GenericBean {
 	public void setValoracion(int valoracion) {
 		this.valoracion = valoracion;
 	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	@Override
 	public void process() {
+		Voto voto = new Voto(valoracion, "cualquierString", NivelEstudios.PRIMARIOS);
+		ControllerFactory.setFactory(new ControllerServletFactory());
+    	ControllerFactory.getFactory().getVotarController().votar(id, voto);
 	}
 
 }
