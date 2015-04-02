93311d56463b7d9f115cf3fa8e5769bfb8bd5e7f
becario92 <fjalva01@gmail.com>
2015-04-02 18:07:14 +0200

    Update TemaResource (eliminar tema)

--- src/main/java/ws/rest/TemaResource.java
+++ src/main/java/ws/rest/TemaResource.java
@@ -4,9 +4,11 @@ import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -20,22 +22,29 @@ import ws.TemaUris;
 public class TemaResource {
 
 	@POST
-    @Consumes(MediaType.APPLICATION_XML)
-    public Response create(Tema tema) {
-        Response result;
-        DaoFactory.setFactory(new DaoJpaFactory());
-        DaoFactory.getFactory().getTemaDao().create(tema);
-        result = Response.created(URI.create(TemaUris.PATH_TEMAS + "/" + tema.getId()))
-                .entity(String.valueOf(tema.getId())).build();
-        return result;
-    }
-	
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response create(Tema tema) {
+		Response result;
+		DaoFactory.setFactory(new DaoJpaFactory());
+		DaoFactory.getFactory().getTemaDao().create(tema);
+		result = Response.created(URI.create(TemaUris.PATH_TEMAS + "/" + tema.getId()))
+				.entity(String.valueOf(tema.getId())).build();
+		return result;
+	}
+
 	@GET
-    @Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
 	public List<Tema> obtenerListaTemas() {
 		DaoFactory.setFactory(new DaoJpaFactory());
-    	List<Tema> listaTemas = DaoFactory.getFactory().getTemaDao().findAll();
-    	return listaTemas;
+		List<Tema> listaTemas = DaoFactory.getFactory().getTemaDao().findAll();
+		return listaTemas;
 	}
-	
+
+	@DELETE
+	@Path(TemaUris.PATH_ID_PARAM)
+	public void delete(@PathParam("id") Integer id) {
+		DaoFactory.setFactory(new DaoJpaFactory());
+    	DaoFactory.getFactory().getTemaDao().deleteById(id);
+	}
+
 }
