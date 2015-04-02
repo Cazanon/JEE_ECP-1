742086c107ee196eb6fd242d510a69a4b63f5dcf
becario92 <fjalva01@gmail.com>
2015-04-02 19:03:53 +0200

    Update VotoResource (obtener niveles de estudios)

--- src/main/java/ws/rest/VotoResource.java
+++ src/main/java/ws/rest/VotoResource.java
@@ -1,5 +1,27 @@
 package ws.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import persistence.models.utils.NivelEstudios;
+import ws.VotoUris;
+
 public class VotoResource {
+	
+	@GET
+    @Path(VotoUris.PATH_NIVELES_ESTUDIOS)
+    @Produces(MediaType.APPLICATION_XML)
+	public List<String> getNivelesEstudios() {
+		List<String> nivelesEstudios = new ArrayList<String>();
+		for(NivelEstudios nivelEstudios : NivelEstudios.values()) {
+			nivelesEstudios.add(nivelEstudios.name());
+		}
+		return nivelesEstudios;
+	}
 
 }
