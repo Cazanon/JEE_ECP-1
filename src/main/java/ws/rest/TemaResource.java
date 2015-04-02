aa5eaaac38c242ae963b59de9a13dc275bf9920e
becario92 <fjalva01@gmail.com>
2015-04-02 15:59:14 +0200

    Update TemaResource (metodo create)

--- src/main/java/ws/rest/TemaResource.java
+++ src/main/java/ws/rest/TemaResource.java
@@ -1,5 +1,30 @@
 package ws.rest;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.jpa.DaoJpaFactory;
+import persistence.models.entities.Tema;
+import ws.TemaUris;
+
+@Path(TemaUris.PATH_TEMAS)
 public class TemaResource {
 
+	@POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response create(Tema tema) {
+        Response result;
+        DaoFactory.setFactory(new DaoJpaFactory());
+        DaoFactory.getFactory().getTemaDao().create(tema);
+        result = Response.created(URI.create(TemaUris.PATH_TEMAS + "/" + tema.getId()))
+                .entity(String.valueOf(tema.getId())).build();
+        return result;
+    }
+	
 }
