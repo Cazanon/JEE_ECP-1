94ca4d376e75aae9ff8bf19bbfd6481e7985e013
becario92 <fjalva01@gmail.com>
2015-04-02 17:35:05 +0200

    Update TemaControllerWs (obtener lista de temas)

--- src/main/java/controllers/ws/TemaControllerWs.java
+++ src/main/java/controllers/ws/TemaControllerWs.java
@@ -2,15 +2,19 @@ package controllers.ws;
 
 import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+
 import persistence.models.entities.Tema;
+import ws.TemaUris;
 import controllers.TemaController;
 
 public class TemaControllerWs implements TemaController {
 
 	@Override
 	public List<Tema> obtenerListaTemas() {
-		// TODO Auto-generated method stub
-		return null;
+		GenericType<List<Tema>> genericType = new GenericType<List<Tema>>() {
+        };
+		return ControllerWs.buildWebServiceManager(TemaUris.PATH_TEMAS).entities(genericType);
 	}
 
 }
